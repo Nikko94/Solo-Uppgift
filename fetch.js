c8f51acb58df8de34758e4261e068649cc2c8a46
nikko94 <nikkok94@hotmail.com>
2023-12-21 15:54:58 +0100

    first commit

---
+++ fetch.js
@@ -0,0 +1,79 @@
+let createbtn = document.getElementById("createPost-btn");
+let postTab = document.getElementById("makePost");
+let inputTitle = document.getElementById("input-title");
+let inputText = document.getElementById("input-text");
+let inputTags = document.getElementsByClassName("input-tags");
+let postpost = document.getElementById("post-post");
+
+/* posts = []; */
+
+createbtn.addEventListener("click", function () {
+    if (postTab.style.display === "none") {
+        postTab.style.display = "flex";
+        createbtn.innerText = "Close";
+    } else {
+        postTab.style.display = "none";
+        createbtn.innerText = "Create Post";
+    }
+});
+
+fetch('https://dummyjson.com/posts')
+    .then(res => res.json())
+    .then(function (res) {
+        renderPosts(res.posts);
+    });
+
+function renderPosts(posts) {
+    for (let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+
+        let div = document.createElement("div");
+
+        let title = document.createElement("h1");
+        let body = document.createElement("h2")
+        let tags = document.createElement("h3")
+        let reactions = document.createElement("h3")
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        tags.innerText = post.tags;
+        reactions.innerText = post.reactions;
+
+        div.append(title, body, tags, reactions);
+        document.body.append(div);
+    }
+    console.log(inputTitle);
+}
+
+function createPost() {
+    let div = document.createElement("div");
+
+    let title = document.createElement("h1");
+    let body = document.createElement("h2");
+    let tags = document.createElement("h3");
+    /* let reactions = document.createElement("h3") */
+
+    title.innerText = inputTitle.value;
+    body.innerText = inputText.value;
+    tags.innerText = `${inputTags[0].value},${inputTags[1].value},${inputTags[2].value} `
+
+
+    div.append(title, body, tags);
+    document.body.append(div);
+
+    inputTitle.value = "";
+    inputText.value = "";
+    inputTags[0].value = "";
+    inputTags[1].value = "";
+    inputTags[2].value = "";
+    postTab.style.display = "none";
+    createbtn.innerText = "Create Post";
+    console.log(inputTags);
+}
+
+postpost.addEventListener("click", function () {
+    createPost();
+})
+
+renderPosts(posts);
+
