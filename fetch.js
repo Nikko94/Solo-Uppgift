3244823450010c61b2d5830fd53f6abc96a059f7
Nikko94 <nikkok94@hotmail.com>
2023-12-23 22:35:42 +0100

    Post

--- fetch.js
+++ fetch.js
@@ -5,7 +5,9 @@ let inputText = document.getElementById("input-text");
 let inputTags = document.getElementsByClassName("input-tags");
 let postpost = document.getElementById("post-post");
 
-/* posts = []; */
+const keyName = "myPost";
+let saveData = [];
+
 
 createbtn.addEventListener("click", function () {
     if (postTab.style.display === "none") {
@@ -17,11 +19,7 @@ createbtn.addEventListener("click", function () {
     }
 });
 
-fetch('https://dummyjson.com/posts')
-    .then(res => res.json())
-    .then(function (res) {
-        renderPosts(res.posts);
-    });
+
 
 function renderPosts(posts) {
     for (let i = 0; i < posts.length; i++) {
@@ -42,8 +40,13 @@ function renderPosts(posts) {
         div.append(title, body, tags, reactions);
         document.body.append(div);
     }
-    console.log(inputTitle);
 }
+
+fetch('https://dummyjson.com/posts')
+    .then(res => res.json())
+    .then(function (res) {
+        renderPosts(res.posts);
+    });
 
 function createPost() {
     let div = document.createElement("div");
@@ -57,6 +60,12 @@ function createPost() {
     body.innerText = inputText.value;
     tags.innerText = `${inputTags[0].value},${inputTags[1].value},${inputTags[2].value} `
 
+    saveData.push({
+        title: inputTitle.value,
+        body: inputText.value,
+        tags: [inputTags[0].value, inputTags[1].value, inputTags[2].value]
+    });
+    
 
     div.append(title, body, tags);
     document.body.append(div);
@@ -68,12 +77,53 @@ function createPost() {
     inputTags[2].value = "";
     postTab.style.display = "none";
     createbtn.innerText = "Create Post";
-    console.log(inputTags);
+
+    storeData("myPost", saveData);
 }
 
-postpost.addEventListener("click", function () {
+postpost.addEventListener("click", () => {
     createPost();
+
 })
 
-renderPosts(posts);
+
+function storeData(key, data) {
+    const json = JSON.stringify(data);
+    localStorage.setItem(key, json);
+}
+
+
+function getLocalStorage(key) {
+    const json = localStorage.getItem(key);
+    const data = JSON.parse(json);
+    return data;
+}
+
+function renderLocalStorage() {
+
+    let storedData = getLocalStorage("myPost");
+
+    storedData.forEach(object => {
+        let div = document.createElement("div");
+        let title = document.createElement("h1");
+        let body = document.createElement("h2")
+        let tags = document.createElement("h3")
+        let reactions = document.createElement("h3")
+
+        title.innerText = object.title;
+        body.innerText = object.body;
+        tags.innerText = object.tags;
+        reactions.innerText = object.reactions;
+
+        div.append(title, body, tags, reactions);
+        document.body.append(div);
+
+    });
+}
+
+renderLocalStorage();
+
+console.log(saveData);
+
+
 
